d473c1bd5f583ece6f468d58e4211cdb65a50ab9
Henry Zeitler <hzeitler@menadwork.com>
2015-03-23 23:33:24 +0100

    prepare game engine

--- js/app.js
+++ js/app.js
@@ -1,2 +1,23 @@
+/**
+* Curcooma! Game Engine
+*
+* @desc This is the main function representing the game engine of Curcooma!. 
+* @author [@Freizeitler]
+* @dependency jQuery, ...
+*/
+
+$.fn.curcooma = function(playerGender, playerType, restaurantRevenue) {
+	var settings = {
+		user: {
+			gender: playerGender,
+			type: playerType
+		},
+		revenue: restaurantRevenue
+	}
+
+};
+
+// init Curcooma!
 $(document).ready(function() {
+	$('body').curcooma();
 });
