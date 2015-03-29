b2578d6dcbf2302e93d43fb74d2e902ed762bed0
Henry Zeitler <hzeitler@menadwork.com>
2015-03-29 17:55:49 +0200

    content area, alerts

--- js-min/app.js
+++ js-min/app.js
@@ -1,6 +1,6 @@
 /*
- * boilerplate by menadwork kommunikation GmbH (http://www.menadwork.com) (2015-03-28, 11:16)
+ * boilerplate by menadwork kommunikation GmbH (http://www.menadwork.com) (2015-03-29, 14:10)
  */
 
-$.fn.curcooma=function(){!function(){$.ajax({async:!1,global:!1,url:"../json/player-data.json",dataType:"json",success:function(a){console.log(a),playerData=a.player,restaurantData=a.restaurant,gameData=a}})}();switch($(".player-name").text(playerData.name),$(".player-level").text(playerData.level),playerData.type){case 1:playerPic="https://polymer-tut.appspot.com/images/avatar-01.svg";break;case 2:playerPic="https://polymer-tut.appspot.com/images/avatar-07.svg";break;case 3:playerPic="https://polymer-tut.appspot.com/images/avatar-03.svg";break;case 4:playerPic="https://polymer-tut.appspot.com/images/avatar-06.svg";break;case 5:playerPic="https://polymer-tut.appspot.com/images/avatar-08.svg";break;default:playerPic="https://polymer-tut.appspot.com/images/avatar-05.svg"}$(".player-pic img").attr("src",playerPic),$(".restaurant-name").text(restaurantData.name),$(".restaurant-money").text(gameData.balance),$(".restaurant-seats").text(restaurantData.seats)},$(document).ready(function(){$("body").curcooma()});
+$(document).ready(function(){var a=$(".navi-opener"),b=$(".navigation");a.on("click",function(){b.hasClass("open")?b.removeClass("open"):b.addClass("open")})});
 //# sourceMappingURL=app.js.map
--- js/app.pkgd.js
+++ js/app.pkgd.js
@@ -1,68 +1,20 @@
 /**
- * Curcooma! Game Engine
+ * App scripts
  *
- * @desc This is the main function representing the game engine of Curcooma!.
+ * @desc Scripts to run the frotend
  * @author [@Freizeitler]
  * @dependency jQuery, ...
  */
 
-$.fn.curcooma = function() {
-
-
-		// consume game-data.json
-		var getGameData = (function() {
-					$.ajax({
-						'async': false,
-						'global': false,
-						'url': '../json/player-data.json',
-						'dataType': "json",
-						'success': function(data) {
-								console.log(data);
-								playerData = data.player;
-								restaurantData = data.restaurant;
-								gameData = data;
-						}
-					});
-				})();
-
-		// populate player section
-		$('.player-name').text(playerData.name);
-		$('.player-level').text(playerData.level);
-		switch (playerData.type) {
-			case 1:
-				playerPic = 'https://polymer-tut.appspot.com/images/avatar-01.svg';
-				break;
-			case 2:
-				playerPic = 'https://polymer-tut.appspot.com/images/avatar-07.svg';
-				break;
-			case 3:
-				playerPic = 'https://polymer-tut.appspot.com/images/avatar-03.svg';
-				break;
-			case 4:
-				playerPic = 'https://polymer-tut.appspot.com/images/avatar-06.svg';
-				break;
-			case 5:
-				playerPic = 'https://polymer-tut.appspot.com/images/avatar-08.svg';
-				break;
-			default:
-				playerPic = 'https://polymer-tut.appspot.com/images/avatar-05.svg';
-		}
-		$('.player-pic img').attr('src', playerPic);
-
-		// populate restaurant info
-		$('.restaurant-name').text(restaurantData.name);
-		$('.restaurant-money').text(gameData.balance);
-		$('.restaurant-seats').text(restaurantData.seats);
-
-		// populate main section
-
-
-
-};
-
-// init Curcooma!
-$(document).ready(function() {
-	$('body').curcooma();
-});
-
+ $(document).ready(function() {
+ 		var opener = $('.navi-opener'),
+ 		target = $('.navigation');
+ 		opener.on('click', function() {
+ 			if (!target.hasClass('open')) {
+ 				target.addClass('open');
+	 		} else {
+	 			target.removeClass('open');
+	 		}
+ 		});
+ });
 //# sourceMappingURL=app.pkgd.js.map
