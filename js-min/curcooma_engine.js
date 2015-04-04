6a4286dbe3dafc0cbc2c7459f4b2f4da85c07c43
Henry Zeitler <hzeitler@menadwork.com>
2015-04-04 20:15:03 +0200

    more routing

--- js-min/curcooma_engine.js
+++ js-min/curcooma_engine.js
@@ -1,5 +1,5 @@
 /*
- * boilerplate by menadwork kommunikation GmbH (http://www.menadwork.com) (2015-04-04, 01:25)
+ * boilerplate by menadwork kommunikation GmbH (http://www.menadwork.com) (2015-04-04, 16:33)
  */
 
 $.fn.curcooma=function(){!function(){$.ajax({async:!1,global:!1,url:"../json/player-data.json",dataType:"json",success:function(a){console.log(a),playerData=a.player,restaurantData=a.restaurant,gameData=a}})}();switch($(".player-name").text(playerData.name),$(".player-level").text(playerData.level),playerData.type){case 1:playerPic="https://polymer-tut.appspot.com/images/avatar-01.svg";break;case 2:playerPic="https://polymer-tut.appspot.com/images/avatar-07.svg";break;case 3:playerPic="https://polymer-tut.appspot.com/images/avatar-03.svg";break;case 4:playerPic="https://polymer-tut.appspot.com/images/avatar-06.svg";break;case 5:playerPic="https://polymer-tut.appspot.com/images/avatar-08.svg";break;default:playerPic="https://polymer-tut.appspot.com/images/avatar-05.svg"}$(".player-pic img").attr("src",playerPic),$(".restaurant-name").text(restaurantData.name),$(".restaurant-money").text(gameData.balance),$(".restaurant-seats").text(restaurantData.seats)},$(document).ready(function(){$("body").curcooma()});
--- js/app.js
+++ js/app.js
@@ -5,26 +5,24 @@
  * @author [@Freizeitler]
  * @dependency jQuery, ...
  */
-
- $(document).ready(function() {
- 		var opener = $('.navi-opener'),
- 		target = $('.navigation');
- 		opener.on('click', function() {
- 			if (!target.hasClass('open')) {
- 				target.addClass('open');
-	 		} else {
-	 			target.removeClass('open');
-	 		}
- 		});
- 		$(document).mouseup(function (e) {
-	    var container = $('.sidebar');
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-	        target.removeClass('open');
-	    }
-		});
- });
-
- /*$(document).ready(function() {
+$(document).ready(function() {
+    var opener = $('.navi-opener'),
+        target = $('.navigation');
+    opener.on('click', function() {
+        if (!target.hasClass('open')) {
+            target.addClass('open');
+        } else {
+            target.removeClass('open');
+        }
+    });
+    $(document).mouseup(function(e) {
+        var container = $('.sidebar');
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            target.removeClass('open');
+        }
+    });
+});
+/*$(document).ready(function() {
  		var clickTarget = $('.content-nav li');
  		clickTarget.on('click', function() {
  			clickTarget.removeClass('active');
